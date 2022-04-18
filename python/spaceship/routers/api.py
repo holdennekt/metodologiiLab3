83872f724dbe0acb7f77f4509dda6aa15258f218
holdennekt <ngryshchak@gmail.com>
2022-04-18 19:24:49 +0300

    created endpoint api/matrix

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,7 @@
+from itertools import product
+import json
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +9,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def randMatrix() -> dict:
+    matrix1 = np.random.rand(10, 10)
+    matrix2 = np.random.rand(10, 10)
+    product = np.matmul(matrix1, matrix2)
+    return {
+        "matrix1": json.dumps(matrix1.tolist()),
+        "matrix2": json.dumps(matrix2.tolist()),
+        "product": json.dumps(product.tolist())
+    }
